c6ac0449fa299ad77974f024c024773ba0cafaad
sunkay <sunkay11@gmail.com>
2017-11-19 10:35:29 -0500

    reverseInt

--- reverseint/reverseint.go
+++ reverseint/reverseint.go
@@ -10,6 +10,35 @@
 
 package main
 
+import (
+	"strconv"
+	"strings"
+)
+
 func reverseInt(v int) int {
-	return v
+	// handle zero
+	if v == 0 {
+		return v
+	}
+
+	isNegative := (v < 0)
+
+	valueStr := strconv.Itoa(v)
+
+	// trim trailing zero's
+	valueStr = strings.Trim(valueStr, "0-")
+
+	// reverse string
+	revValueStr := ""
+	for i := 0; i < len(valueStr); i++ {
+		revValueStr = string(valueStr[i]) + revValueStr
+	}
+
+	revValue, _ := strconv.Atoi(revValueStr)
+
+	if isNegative {
+		revValue = -revValue
+	}
+
+	return revValue
 }
