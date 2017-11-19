276a15e9aa57573893e8dfdb0109ebc1c3af270f
sunkay <sunkay11@gmail.com>
2017-11-18 20:40:30 -0500

    .

--- reversestring/main_test.go
+++ reversestring/main_test.go
@@ -3,11 +3,11 @@ package main
 import "testing"
 
 func TestReverse(t *testing.T) {
-	if reverse("abcd") != "dcba" {
-		t.Errorf("Expected dcba but got %v", reverse("abcd"))
+	if r := reverse("abcd"); r != "dcba" {
+		t.Errorf("Expected dcba but got %v", r)
 	}
 
-	if reverse("  abcd") != "dcba  " {
-		t.Errorf("Expected dcba__ but got %v", reverse("abcd  "))
+	if r := reverse("  abcd"); r != "dcba  " {
+		t.Errorf("Expected dcba__ but got %v", r)
 	}
 }
