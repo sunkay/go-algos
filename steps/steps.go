e18b2771aaceb654e5167ff58f38cf8833e6de95
Kayiti <skayiti@kayitimbp.lan>
2018-03-01 08:06:24 -0500

    .

--- steps/steps.go
+++ steps/steps.go
@@ -5,12 +5,17 @@
 // step has spaces on the right hand side!
 // --- Examples
 //   steps(2)
-//       '# '
-//       '##'
+//     0  '# '
+//		   01
+//     1  '##'
+//	       01
 //   steps(3)
-//       '#  '
-//       '## '
-//       '###'
+//     0  '#  '
+//         012
+//     1  '## '
+//         012
+//     2  '###'
+//         012
 //   steps(4)
 //       '#   '
 //       '##  '
@@ -19,7 +24,25 @@
 
 package steps
 
-func steps(count int, row int) []string {
+import (
+	"fmt"
+)
 
-	return nil
+func steps(count int) []string {
+	var steps []string
+
+	for i := 0; i < count; i++ {
+		step := ""
+		for j := 0; j < count; j++ {
+			if i >= j {
+				step += "#"
+			} else {
+				step += " "
+			}
+		}
+		fmt.Println(step)
+		steps = append(steps, step)
+	}
+
+	return steps
 }
